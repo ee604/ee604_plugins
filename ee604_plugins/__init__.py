48bc8024f3b29939cd9f1897e121c73d9cec9991
shashikg <shashikg.iitk@gmail.com>
2020-09-05 11:20:53 +0530

    add overwrite option to download dataset function

--- ee604_plugins/__init__.py
+++ ee604_plugins/__init__.py
@@ -7,7 +7,7 @@ def download_dataset(assignment_no=0, task_no=3):
     if (assignment_no in assignment_no_list) and (task_no in task_no_list):
         url = "https://github.com/ee604/ee604_assignments/raw/master/assignment_" + str(assignment_no) + "/data/task_" + str(task_no) + ".zip"
         subprocess.check_output(["wget", "-O", "data.zip", url])
-        subprocess.check_output(["unzip", "data.zip", "-d", "./data/"])
+        subprocess.check_output(["unzip", "-o", "data.zip", "-d", "./data/"])
         print("Download Complete!")
     else:
         print("No dataset available for assignment", assignment_no, ", task no", task_no)
