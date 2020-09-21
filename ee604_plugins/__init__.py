0b5c6bc9f625775faf91c17e5e00a18eba5eb0af
shashikg <shashikg.iitk@gmail.com>
2020-09-21 08:41:09 +0530

    added video player

--- ee604_plugins/__init__.py
+++ ee604_plugins/__init__.py
@@ -11,6 +11,10 @@ from PIL import Image
 import numpy as np
 import subprocess
 import os
+import io
+
+from base64 import b64decode, b64encode
+from IPython.display import HTML
 
 assignment_no_list = [0, 1]
 task_no_list = [[3], [2, 3]]
@@ -46,3 +50,12 @@ def cv2_imshow(a):
   display.display(Image.fromarray(a))
 
 cv_imshow = cv2_imshow
+
+def playVideo(filename, format='mp4', width=640, height=480):
+    encoded = b64encode(io.open(filename, 'r+b').read())
+    embedded = HTML('''
+        <video width="{1}" height="{2}" controls>
+            <source src="data:video/{3};base64,{0}" type="video/{3}" />
+        </video>'''.format(encoded.decode('utf-8'), width, height, format))
+
+    display.display(embedded)
